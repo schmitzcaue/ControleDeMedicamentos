fe9275da6d0d7f6e71bb5405410f3bd7bedbadb1
caues <caues@SCHMITZNOT>
2025-08-18 00:53:03 -0300

    Implementado todo o crud de medicamentos com um bug em repositorioModuloMedicamento (No controller)

---
+++ ControleDeMedicamentos.WebApp/Controllers/FornecedorController.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+
+//namespace ControleDeMedicamentos.WebApp.Controllers
+//{
+//    public FornecedorController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//    }
+//}
