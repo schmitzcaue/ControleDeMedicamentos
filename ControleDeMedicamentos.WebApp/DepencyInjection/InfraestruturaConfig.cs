0a185173e1cf871233fca81d52de47403e0c9a6c
caues <caues@SCHMITZNOT>
2025-09-23 21:07:03 -0300

    Implementado RepositorioPrescricaoEmSql e Tabelas

---
+++ ControleDeMedicamentos.WebApp/DepencyInjection/InfraestruturaConfig.cs
@@ -0,0 +1,41 @@
+﻿using ControleDeMedicamentos.Infraestrutura.Arquivo.ModuloPrescricao;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.Compartilhado;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloMedicamento;
+using ControleDeMedicamentos.Infraestrutura.Arquivo.ModuloPrescricao;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloRequisicaoMedicamento;
+using ControleDeMedicamentos.Infraestrutura.SqlServer.ModuloFornecedor;
+using ControleDeMedicamentos.Infraestrutura.SqlServer.ModuloFuncionario;
+using ControleDeMedicamentos.Infraestrutura.SqlServer.ModuloMedicamento;
+using ControleDeMedicamentos.Infraestrutura.SqlServer.ModuloPaciente;
+using ControleDeMedicamentos.Infraestrutura.SqlServer.ModuloPrescricao;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ControleDeMedicamentos.WebApp.DependencyInjection;
+
+public static class InfraestruturaConfig
+{
+    public static void AddCamadaInfraestrutura(this IServiceCollection services, IConfiguration configuracao)
+    {
+        services.AddScoped<IDbConnection>(_ =>
+        {
+            var connectionString = configuracao["SQL_CONNECTION_STRING"];
+
+            return new SqlConnection(connectionString);
+        });
+
+        services.AddScoped<RepositorioPacienteEmSql>();
+        services.AddScoped<RepositorioFuncionarioEmSql>();
+        services.AddScoped<RepositorioFornecedorEmSql>();
+        services.AddScoped<RepositorioMedicamentoEmSql>();
+        services.AddScoped<RepositorioPrescricaoEmSql>();
+
+        services.AddScoped((_) => new ContextoDados(true));
+        //services.AddScoped<RepositorioPacienteEmArquivo>();
+        //services.AddScoped<RepositorioFuncionarioEmArquivo>();
+        //services.AddScoped<RepositorioFornecedorEmArquivo>();
+        services.AddScoped<RepositorioMedicamentoEmArquivo>();
+        services.AddScoped<RepositorioPrescricaoEmArquivo>();
+        services.AddScoped<RepositorioRequisicaoMedicamentoEmArquivo>();
+    }
+}
